967087e909e3b492646070b059a8936cf39e475b
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-11-02 00:24:06 -0300

    Change some details in title and added a bird image

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './title.css'
+import BirdImage from '../../assets/img/bird.png'
 
 function Title() {
   return (
     <header className="header">
       <div className="header-container">
-        <h1>Tasking</h1>
+        <h1>Hear Story</h1>
+        <img src={BirdImage} alt="Singing Bird" />
       </div>
     </header>
   )
